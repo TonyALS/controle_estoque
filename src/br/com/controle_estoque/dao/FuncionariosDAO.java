beb8c96b8853b94f03098585b5e3abfd20eeea91
TonyALS <tony_luzsilva@hotmail.com>
2019-09-17 22:08:29 -0300

    Finaliza o método cadastrarFuncionario

--- src/br/com/controle_estoque/dao/FuncionariosDAO.java
+++ src/br/com/controle_estoque/dao/FuncionariosDAO.java
@@ -33,7 +33,7 @@ public class FuncionariosDAO {
             
             //1º Passo: Criar a query SQL:
             String sql = "INSERT INTO tb_funcionarios (nome,rg,cpf,email,senha,"
-                    + "cargo,nivel_acesso,telefone,celular,cep,endereco,numero"
+                    + "cargo,nivel_acesso,telefone,celular,cep,endereco,numero,"
                     + "complemento,bairro,cidade,estado)"
                     + " values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
             
@@ -45,15 +45,18 @@ public class FuncionariosDAO {
             stmt.setString(2, obj.getRg());
             stmt.setString(3, obj.getCpf());
             stmt.setString(4, obj.getEmail());
-            stmt.setString(5, obj.getTelefone());
-            stmt.setString(6, obj.getCelular());
-            stmt.setString(7, obj.getCep());
-            stmt.setString(8, obj.getEndereco());
-            stmt.setInt(9, obj.getEnderecoNumero());
-            stmt.setString(10, obj.getComplemento());
-            stmt.setString(11, obj.getBairro());
-            stmt.setString(12, obj.getCidade());
-            stmt.setString(13, obj.getUf());
+            stmt.setString(5, obj.getSenha());
+            stmt.setString(6, obj.getCargo());
+            stmt.setString(7, obj.getNivel_acesso());
+            stmt.setString(8, obj.getTelefone());
+            stmt.setString(9, obj.getCelular());
+            stmt.setString(10, obj.getCep());
+            stmt.setString(11, obj.getEndereco());
+            stmt.setInt(12, obj.getEnderecoNumero());
+            stmt.setString(13, obj.getComplemento());
+            stmt.setString(14, obj.getBairro());
+            stmt.setString(15, obj.getCidade());
+            stmt.setString(16, obj.getUf());
             
             //3º Executar o comando SQL:
             stmt.execute();
