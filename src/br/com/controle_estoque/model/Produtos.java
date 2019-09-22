7e65d835c5960e8d936600e23a825d6fb3d59359
TonyALS <tony_luzsilva@hotmail.com>
2019-09-22 15:04:28 -0300

    Cria a classe Produtos na camada model

---
+++ src/br/com/controle_estoque/model/Produtos.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.controle_estoque.model;
+
+/**
+ *
+ * @author Tony
+ */
+public class Produtos {
+    
+    private int id;
+    private String descricao;
+    private double preco;
+    private int qtdEstoque;
+    
+    //Esse objeto representará a nossa chave estrangeira do tipo fornecedor:
+    private Fornecedores fornecedor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public Fornecedores getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedores fornecedor) {
+        this.fornecedor = fornecedor;
+    }   
+}
