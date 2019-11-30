dfad0812663a1cd33789c6d43178509ee29725fb
TonyALS <tony_luzsilva@hotmail.com>
2019-11-30 10:49:20 -0300

    Cria o método listarVendasPorPeriodo na classe VendasDAO

--- src/br/com/controle_estoque/dao/VendasDAO.java
+++ src/br/com/controle_estoque/dao/VendasDAO.java
@@ -6,11 +6,14 @@
 package br.com.controle_estoque.dao;
 
 import br.com.controle_estoque.jdbc.ConnectionFactory;
+import br.com.controle_estoque.model.Clientes;
 import br.com.controle_estoque.model.Vendas;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 /**
@@ -70,5 +73,46 @@ public class VendasDAO {
         } catch (Exception e) {
             throw new RuntimeException(e); 
         } 
-    }  
+    }
+    
+    //Método filtra vendas por data:
+    public List<Vendas> listarVendasPorPeriodo(String dataInicio, String dataFim) {
+        try {
+
+            //1º Passo: Criar a lista:
+            List<Vendas> lista = new ArrayList<>();
+
+            //2º Passo: Criar o SQL que fará a seleção no BD:
+            String sql = "SELECT v.id, v.data_venda, c.nome, v.total_venda,"
+                    + " v.observacoes FROM tb_vendas AS v INNER JOIN tb_clientes"
+                    + " AS c ON(v.cliente_id = c.id) WHERE v.data_venda"
+                    + " BETWEEN ? AND ?";
+
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, dataInicio);
+            stmt.setString(2, dataFim);
+            
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Vendas obj = new Vendas();
+                Clientes c = new Clientes();
+                
+                obj.setId(rs.getInt("v.id"));
+                obj.setData_venda(rs.getString("v.data_venda"));
+                c.setNome(rs.getString("c.nome"));
+                obj.setTotal_venda(rs.getDouble("v.total_venda"));
+                obj.setObs(rs.getString("v.observacoes"));
+        
+                obj.setCliente(c);
+                
+                lista.add(obj);
+            }
+            return lista;
+
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro: " + e);
+            return null;
+        }
+    }
 }
