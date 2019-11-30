437d2826f0afa20d6fc0c8084b8c51f51e453e25
TonyALS <tony_luzsilva@hotmail.com>
2019-11-30 15:21:15 -0300

    Implementa níveis de acesso dos usuários

--- src/br/com/controle_estoque/dao/FuncionariosDAO.java
+++ src/br/com/controle_estoque/dao/FuncionariosDAO.java
@@ -23,28 +23,27 @@ import javax.swing.JOptionPane;
  * @author Tony
  */
 public class FuncionariosDAO {
-    
+
     private Connection con;
-    
+
     //Para não precisar abrir a conexão em todos os métodos, já inicializamos 
     //uma conexão dentro do próprio Construtor:
-    public FuncionariosDAO(){
+    public FuncionariosDAO() {
         this.con = new ConnectionFactory().getConnection();
     }
-    
+
     //Método cadastrar funcionário:
-     public void cadastrarFuncionario(Funcionarios obj){
-        
+    public void cadastrarFuncionario(Funcionarios obj) {
+
         try {
-            
+
             //1º Passo: Criar a query SQL:
             String sql = "INSERT INTO tb_funcionarios (nome,rg,cpf,email,senha,"
                     + "cargo,nivel_acesso,telefone,celular,cep,endereco,numero,"
                     + "complemento,bairro,cidade,estado)"
                     + " values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-            
+
             //2º Passo: Conectar com o banco de dados e organizar a query sql:
-            
             //Classe PreparedStatement é responsável por tratar os comandos sql:
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.setString(1, obj.getNome());
@@ -63,36 +62,36 @@ public class FuncionariosDAO {
             stmt.setString(14, obj.getBairro());
             stmt.setString(15, obj.getCidade());
             stmt.setString(16, obj.getUf());
-            
+
             //3º Executar o comando SQL:
             stmt.execute();
             //Fecha a conexão com o banco de dados:
             stmt.close();
-            
+
             //Exibe um aviso de cadastrado com sucesso:
             JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
-            
+
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro: " + e);
-            
+
         }
     }
-     
-     //Método listarTodosOsFuncionários:
-    public List<Funcionarios> listarFuncionarios(){
+
+    //Método listarTodosOsFuncionários:
+    public List<Funcionarios> listarFuncionarios() {
         try {
             //1º Criar a lista:
             List<Funcionarios> lista = new ArrayList<>();
-            
+
             //2º Criar query sql e executar:
             String sql = "SELECT * FROM tb_funcionarios";
             PreparedStatement stmt = con.prepareStatement(sql);
-            
+
             //Todo comando select salva o resultado da consulta no BD numa classe
             //chamada ResultSet, ela que utilizaremos para adicionar os itens à lista:
             ResultSet rs = stmt.executeQuery();
-            
-            while(rs.next()){
+
+            while (rs.next()) {
                 Funcionarios obj = new Funcionarios();
                 obj.setId(rs.getInt("id"));
                 obj.setNome(rs.getString("nome"));
@@ -111,32 +110,31 @@ public class FuncionariosDAO {
                 obj.setBairro(rs.getString("bairro"));
                 obj.setCidade(rs.getString("cidade"));
                 obj.setUf(rs.getString("estado"));
-                
+
                 lista.add(obj);
             }
-            
+
             return lista;
-            
+
         } catch (Exception e) {
-            
+
             JOptionPane.showMessageDialog(null, "Erro: " + e);
             return null;
         }
     }
-    
+
     //Método alterarFuncionario:
-    public void alterarFuncionario(Funcionarios obj){
-        
-         try {
-            
+    public void alterarFuncionario(Funcionarios obj) {
+
+        try {
+
             //1º Passo: Criar a query SQL:
             String sql = "UPDATE tb_funcionarios SET nome=?, rg=?, cpf=?, email=?, senha=?,"
                     + " cargo=?, nivel_acesso=?, telefone=?, celular=?, cep=?,"
                     + " endereco=?, numero=?, complemento=?,"
                     + " bairro=?, cidade=?, estado=? WHERE id=?";
-            
+
             //2º Passo: Organizar a query sql:
-            
             //Classe PreparedStatement é responsável por tratar os comandos sql:
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.setString(1, obj.getNome());
@@ -156,65 +154,64 @@ public class FuncionariosDAO {
             stmt.setString(15, obj.getCidade());
             stmt.setString(16, obj.getUf());
             stmt.setInt(17, obj.getId());
-            
+
             //3º Executar o comando SQL:
             stmt.execute();
             //Fecha a conexão com o banco de dados:
             stmt.close();
-            
+
             //Exibe um aviso de alterado com sucesso:
             JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
-            
+
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro ao alterar: " + e);
-            
-        }    
+
+        }
     }
-    
+
     //Método excluirFuncionario:
-    public void excluirFuncionario(Funcionarios obj){
-        
-         try {
-            
+    public void excluirFuncionario(Funcionarios obj) {
+
+        try {
+
             //1º Passo: Criar a query SQL:
             String sql = "DELETE FROM tb_funcionarios WHERE id = ?";
-            
+
             //2º Passo: Conectar com o banco de dados e organizar a query sql:
-            
             //Classe PreparedStatement é responsável por tratar os comandos sql:
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.setInt(1, obj.getId());
-            
+
             //3º Executar o comando SQL:
             stmt.execute();
             //Fecha a conexão com o banco de dados:
             stmt.close();
-            
+
             //Exibe um aviso de excluído com sucesso:
             JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
-            
+
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro ao excluir: " + e);
-            
-        }  
+
+        }
     }
-    
+
     //Método buscar funcionario por nome:
-    public List<Funcionarios> buscaFuncionarioPorNome(String nome){
+    public List<Funcionarios> buscaFuncionarioPorNome(String nome) {
         try {
             //1º Criar a lista:
             List<Funcionarios> lista = new ArrayList<>();
-            
+
             //2º Criar query sql com a busca:
             String sql = "SELECT * FROM tb_funcionarios WHERE nome LIKE ?";
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.setString(1, nome);
-            
+
             //Todo comando select salva o resultado da consulta no BD numa classe
             //chamada ResultSet, ela que utilizaremos para adicionar os itens à lista:
             ResultSet rs = stmt.executeQuery();
-            
-            while(rs.next()){
+
+            while (rs.next()) {
                 Funcionarios obj = new Funcionarios();
                 obj.setId(rs.getInt("id"));
                 obj.setNome(rs.getString("nome"));
@@ -233,26 +230,24 @@ public class FuncionariosDAO {
                 obj.setBairro(rs.getString("bairro"));
                 obj.setCidade(rs.getString("cidade"));
                 obj.setUf(rs.getString("estado"));
-                
+
                 lista.add(obj);
             }
-            
+
             return lista;
-            
+
         } catch (Exception e) {
-            
+
             JOptionPane.showMessageDialog(null, "Erro: " + e);
             return null;
         }
     }
-    
-    
+
     //Busca CEP
-    
     public Funcionarios buscaCep(String cep) {
-       
+
         WebServiceCep webServiceCep = WebServiceCep.searchCep(cep);
-       
+
         Funcionarios obj = new Funcionarios();
 
         if (webServiceCep.wasSuccessful()) {
@@ -268,36 +263,48 @@ public class FuncionariosDAO {
         }
 
     }
-    
+
     //Método de login para os funcionários:
-    public void efetuaLogin(String email, String senha){
+    public void efetuaLogin(String email, String senha) {
         try {
             //1º - SQL
             String sql = "SELECT * FROM tb_funcionarios "
-                        + " WHERE email=? and senha=?";
-            
+                    + " WHERE email=? and senha=?";
+
             PreparedStatement stmt = con.prepareStatement(sql);
             stmt.setString(1, email);
             stmt.setString(2, senha);
-            
+
             ResultSet rs = stmt.executeQuery();
-            
-            if(rs.next()){
-                //Usuário logou, chama a tela principal do programa:
-                FrmMenu tela = new FrmMenu();
-                //usuarioLogado é uma variável public presente na Frmmenu:
-                tela.usuarioLogado = rs.getString("nome");
-                tela.setVisible(true);
-                
-            }else{
+
+            if (rs.next()) {
+                //Usuário admin:
+                if (rs.getString("nivel_acesso").equals("Administrador")) {
+                    //Usuário logou, chama a tela principal do programa:
+                    FrmMenu tela = new FrmMenu();
+                    //usuarioLogado é uma variável public presente na Frmmenu:
+                    tela.usuarioLogado = rs.getString("nome");
+                    tela.setVisible(true);
+                }else if(rs.getString("nivel_acesso").equals("Usuário")){ //Caso não admin
+                    //Usuário logou, chama a tela principal do programa:
+                    FrmMenu tela = new FrmMenu();
+                    //usuarioLogado é uma variável public presente na Frmmenu:
+                    tela.usuarioLogado = rs.getString("nome");
+                    //Desabilita itens não acessíveis a usuários:
+                    tela.menu_controlevendas.setEnabled(false);
+                    tela.menu_posicao.setEnabled(false);
+                    tela.setVisible(true);
+                }
+
+            } else {
                 //Dados incorretos
                 JOptionPane.showMessageDialog(null, "Dados incorretos.");
                 //Abre novamente a tela de login para o usuário inserir as credenciais:
                 new FrmLogin().setVisible(true);
             }
-            
+
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro: " + e);
         }
-    }   
+    }
 }
--- src/br/com/controle_estoque/view/FrmMenu.java
+++ src/br/com/controle_estoque/view/FrmMenu.java
@@ -55,8 +55,8 @@ public class FrmMenu extends javax.swing.JFrame {
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenu4 = new javax.swing.JMenu();
         jMenuItem6 = new javax.swing.JMenuItem();
-        jMenuItem7 = new javax.swing.JMenuItem();
-        jMenuItem8 = new javax.swing.JMenuItem();
+        menu_posicao = new javax.swing.JMenuItem();
+        menu_controlevendas = new javax.swing.JMenuItem();
         jMenu7 = new javax.swing.JMenu();
         jMenuItem9 = new javax.swing.JMenuItem();
         jMenu8 = new javax.swing.JMenu();
@@ -158,11 +158,11 @@ public class FrmMenu extends javax.swing.JFrame {
         jMenuItem6.setText("Abrir PDV");
         jMenu4.add(jMenuItem6);
 
-        jMenuItem7.setText("Posição do dia");
-        jMenu4.add(jMenuItem7);
+        menu_posicao.setText("Posição do dia");
+        jMenu4.add(menu_posicao);
 
-        jMenuItem8.setText("Histórico de vendas");
-        jMenu4.add(jMenuItem8);
+        menu_controlevendas.setText("Histórico de vendas");
+        jMenu4.add(menu_controlevendas);
 
         jMenuBar1.add(jMenu4);
 
@@ -257,11 +257,11 @@ public class FrmMenu extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JMenuItem jMenuItem7;
-    private javax.swing.JMenuItem jMenuItem8;
     private javax.swing.JMenuItem jMenuItem9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel labelUsuario;
+    public javax.swing.JMenuItem menu_controlevendas;
+    public javax.swing.JMenuItem menu_posicao;
     private javax.swing.JDesktopPane painelDesktop;
     // End of variables declaration//GEN-END:variables
 }
